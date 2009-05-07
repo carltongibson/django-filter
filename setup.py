a94c82164968d8313eeec45ea3d1ce78e64fe8dd
Jannis Leidel <jannis@leidel.info>
2009-05-08 03:48:05 +0800

    Added additional manifest template to make sure distutils likes the package. Got rid of dependency on setuptools_git, too
    Signed-off-by: Alex Gaynor <alex.gaynor@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-filter',
-    version='0.1.0',
+    version='0.1.1',
     description='Django-filter is a reusable Django application for allowing users to filter queryset dynamically.',
     author='Alex Gaynor',
     author_email='alex.gaynor@gmail.com',
@@ -17,11 +17,13 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    # Make setuptools include all data files under version control,
-    # svn and CVS by default
-    include_package_data=True,
-    # Tells setuptools to download setuptools_git before running setup.py so
-    # it can find the data files under Git version control.
-    setup_requires=['setuptools_git'],
+    package_data = {
+        'filter': [
+            'fixtures/*.json',
+        ],
+        'filter.tests': [
+            'templates/filter/*.html',
+        ]
+    },
     zip_safe=False,
 )
