88ca1420beedc898f7bce54adaee2e44389d7910
Sidorenko Vladimir <yoyavova@gmail.com>
2009-09-19 04:32:08 +0800

    in view check if request.GET is not empty

--- django_filters/views.py
+++ django_filters/views.py
@@ -13,7 +13,7 @@ def object_filter(request, model=None, queryset=None, template_name=None, extra_
         meta = type('Meta', (object,), {'model': model})
         filter_class = type('%sFilterSet' % model._meta.object_name, (FilterSet,),
             {'Meta': meta})
-    filterset = filter_class(request.GET, queryset=queryset)
+    filterset = filter_class(request.GET or None, queryset=queryset)
 
     if not template_name:
         template_name = '%s/%s_filter.html' % (model._meta.app_label, model._meta.object_name.lower())
