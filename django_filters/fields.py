0339047e733a014b88067ef7c96ad74833776fdb
shelldweller <s.kuzmenko@gmail.com>
2011-02-03 21:54:32 -0500

    forgot newline at the EOF
    This reverts commit a4e8f06c6c830299317099af67d3f79541fb7c8f.

--- django_filters/fields.py
+++ django_filters/fields.py
@@ -2,39 +2,21 @@ from django import forms
 
 from django_filters.widgets import RangeWidget, LookupTypeWidget
 
-class BaseRangeField(forms.MultiValueField):
-    """
-    Base abstract class for range filters. Inheriting classes must
-    define form_field attribure which is a form field class to be used for
-    validation.
-    """
+class RangeField(forms.MultiValueField):
     widget = RangeWidget
-    form_class = None
+
     def __init__(self, *args, **kwargs):
         fields = (
-            self.form_class(),
-            self.form_class(),
+            forms.DecimalField(),
+            forms.DecimalField(),
         )
-        super(BaseRangeField, self).__init__(fields, *args, **kwargs)
+        super(RangeField, self).__init__(fields, *args, **kwargs)
 
     def compress(self, data_list):
         if data_list:
             return slice(*data_list)
         return None
 
-class NumericRangeField(BaseRangeField):
-    form_class = forms.DecimalField
-
-class DateRangeField(BaseRangeField):
-    form_class = forms.DateField
-
-class TimeRangeField(BaseRangeField):
-    form_class = forms.TimeField
-
-class RangeField(forms.MultiValueField):
-    """Deprecated. Use NumericRangeField instead."""
-    form_class = forms.DecimalField
-    
 class LookupTypeField(forms.MultiValueField):
     def __init__(self, field, lookup_choices, *args, **kwargs):
         fields = (
