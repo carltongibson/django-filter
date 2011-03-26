d3c6c4960de0b24f4924ccda5224a9805864169f
yeago <yeago@tappedout.net>
2011-03-26 14:03:56 -0500

    added 'page_length' to view to bring it more in line with django generics

--- django_filters/views.py
+++ django_filters/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import Http404
+
+from django.core.paginator import EmptyPage
 
 from django_filters.filterset import FilterSet
 
 def object_filter(request, model=None, queryset=None, template_name=None, extra_context=None,
-    context_processors=None, filter_class=None):
+    context_processors=None, filter_class=None, page_length=None, page_variable="p"):
     if model is None and filter_class is None:
         raise TypeError("object_filter must be called with either model or filter_class")
     if model is None:
@@ -25,4 +28,22 @@ def object_filter(request, model=None, queryset=None, template_name=None, extra_
             if callable(v):
                 v = v()
             c[k] = v
+
+    if page_length:
+	    from django.core.paginator import Paginator
+	    p = Paginator(filterset.qs,page_length)
+            getvars = request.GET.copy()
+            if page_variable in getvars:
+                del getvars[page_variable]
+
+            if len(getvars.keys()) > 0:
+                p.querystring = "&%s" % getvars.urlencode()
+
+            try:
+	        c['paginated_filter'] = p.page(request.GET.get(page_variable,1))
+            except EmptyPage:
+                raise Http404
+
+	    c['paginator'] = p
+
     return render_to_response(template_name, c)
