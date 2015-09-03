d71fd50724871f25e7f2915bd4d65851b6ec61a3
Carlton Gibson <carlton.gibson@noumenal.co.uk>
2015-09-03 14:23:06 +0200

    Update setup.py
    Fixes #282

--- setup.py
+++ setup.py
@@ -9,8 +9,14 @@ f.close()
 version = '0.11.0'
 
 if sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist upload")
-    os.system("python setup.py bdist_wheel upload")
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
     print("You probably want to also tag the version now:")
     print("  git tag -a %s -m 'version %s'" % (version, version))
     print("  git push --tags")
