a5fb0850fb5900d6c553490949998ffad4e9f058
Ryan P Kilby <rpkilby@ncsu.edu>
2015-11-23 03:06:24 -0500

    Consolidate shell/test settings, replace syncdb w/ migrate

--- runshell.py
+++ runshell.py
@@ -1,28 +1,12 @@
 #!/usr/bin/env python
+import os
 import sys
-from django.conf import settings
 from django.core.management import execute_from_command_line
-
-if not settings.configured:
-    settings.configure(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': ':memory:',
-            },
-        },
-        INSTALLED_APPS=(
-            'django_filters',
-            'tests',
-        ),
-        ROOT_URLCONF=None,
-        USE_TZ=True,
-        SECRET_KEY='foobar'
-    )
 
 
 def runshell():
-    execute_from_command_line(sys.argv[:1] + ['syncdb', '--noinput', '-v', '0'])
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    execute_from_command_line(sys.argv[:1] + ['migrate', '--noinput', '-v', '0'])
     argv = sys.argv[:1] + ['shell'] + sys.argv[1:]
     execute_from_command_line(argv)
 
