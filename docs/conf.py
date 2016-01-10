48f34c9b4ff5c5d9be48cac5ebf4f721519a7fd7
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-01-10 05:29:01 +0100

    Update indexs in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('asd', 'django-filter.tex', u'django-filter Documentation',
+  ('index', 'django-filter.tex', u'django-filter Documentation',
    u'Alex Gaynor and others.', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('asd', 'django-filter', u'django-filter Documentation',
+    ('index', 'django-filter', u'django-filter Documentation',
      [u'Alex Gaynor and others.'], 1)
 ]
 
@@ -227,7 +227,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('asd', 'django-filter', u'django-filter Documentation',
+  ('index', 'django-filter', u'django-filter Documentation',
    u'Alex Gaynor and others.', 'django-filter', 'One line description of project.',
    'Miscellaneous'),
 ]
