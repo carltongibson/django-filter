b2a9361e8d8effe1ff073d618a5a5122a7c6319f
Ryan P Kilby <rpkilby@ncsu.edu>
2017-05-26 16:34:02 -0700

    Remove rest_framework sub-package import

--- django_filters/__init__.py
+++ django_filters/__init__.py
@@ -4,12 +4,6 @@ from .constants import STRICTNESS
 from .filterset import FilterSet
 from .filters import *
 
-# We make the `rest_framework` module available without an additional import.
-#   If DRF is not installed we simply set None.
-try:
-    from . import rest_framework
-except ImportError:
-    rest_framework = None
 
 __version__ = '1.0.4'
 
