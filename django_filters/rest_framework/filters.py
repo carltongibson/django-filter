7bc736b9a31d532930e9824842f7adb84436a7b8
Ollie Ford <dev@ojford.com>
2017-11-25 12:57:49 +0000

    Fix mypy error: circular inheritance
    Closes #832, #833

--- django_filters/rest_framework/filters.py
+++ django_filters/rest_framework/filters.py
@@ -1,9 +1,10 @@
 
+from ..filters import BooleanFilter as _BooleanFilter
 from ..filters import *
 from ..widgets import BooleanWidget
 
 
-class BooleanFilter(BooleanFilter):
+class BooleanFilter(_BooleanFilter):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('widget', BooleanWidget)
 
