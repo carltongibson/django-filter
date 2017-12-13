ee29bf099f55326ec6870d246fbcdc777bae61c3
Oskar Persson <oskar.persson@polken.se>
2017-12-13 11:09:26 +0100

    Use DRF BooleanFilter for NullBooleanField

--- tests/models.py
+++ tests/models.py
@@ -51,6 +51,7 @@ class User(models.Model):
     status = models.IntegerField(choices=STATUS_CHOICES, default=0)
 
     is_active = models.BooleanField(default=False)
+    is_employed = models.NullBooleanField(default=False)
 
     favorite_books = models.ManyToManyField('Book', related_name='lovers')
 
