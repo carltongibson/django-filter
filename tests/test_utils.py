carltongibson/django-filter

commit 78febd15cf975b924d47e88e1639e935bbfe5632

Rework validation, add queryset filter method (#788)
* Extract validation and filtering from caching

* Remove STRICTNESS, handle strictness in views

- Remove 'FILTERS_STRICTNESS' setting.
- Remove 'strict' option from FilterSet.
- Add 'raise_exception' class attribute to DjangoFilterBackend.
- Add 'strict' and 'get_strict' to FilterMixin.

* Add 'is_valid()' and 'errors' to FilterSet API

* Add FilterSet.get_form_class()

* Remove strictness from tests

* Drop FilterSet strict/together docs

* Add note on overriding FilterSet classmethods

* Add FilterSet API tests

* Add tests for view strictness

* Rework 'raw_validation' util

* Update filter backend+tests

django_filters/utils.py
@@ -1,4 +1,5 @@
 import warnings
+from collections import OrderedDict
 
 import django
 from django.conf import settings
@@ -8,7 +9,6 @@ from django.db.models.constants import LOOKUP_SEP
 from django.db.models.expressions import Expression
 from django.db.models.fields import FieldDoesNotExist
 from django.db.models.fields.related import ForeignObjectRel, RelatedField
-from django.forms import ValidationError
 from django.utils import timezone
 from django.utils.encoding import force_text
 from django.utils.text import capfirst
@@ -233,22 +233,17 @@ def label_for_filter(model, field_name, lookup_expr, exclude=False):
     return verbose_expression
 
 
-def raw_validation(error):
+def translate_validation(error_dict):
     """
-    Deconstruct a django.forms.ValidationError into a primitive structure
-    eg, plain dicts and lists.
+    Translate a Django ErrorDict into its DRF ValidationError.
     """
-    if isinstance(error, ValidationError):
-        if hasattr(error, 'error_dict'):
-            error = error.error_dict
-        elif not hasattr(error, 'message'):
-            error = error.error_list
-        else:
-            error = error.message
+    # it's necessary to lazily import the exception, as it can otherwise create
+    # an import loop when importing django_filters inside the project settings.
+    from rest_framework.exceptions import ValidationError, ErrorDetail
 
-    if isinstance(error, dict):
-        return {key: raw_validation(value) for key, value in error.items()}
-    elif isinstance(error, list):
-        return [raw_validation(value) for value in error]
-    else:
-        return error
+    exc = OrderedDict(
+        (key, [ErrorDetail(e.message, code=e.code) for e in error_list])
+        for key, error_list in error_dict.as_data().items()
+    )
+
+    return ValidationError(exc)

tests/test_utils.py
@@ -3,20 +3,19 @@ import datetime
 from django.db import models
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.fields.related import ForeignObjectRel
-from django.forms import ValidationError
 from django.test import TestCase, override_settings
 from django.utils.functional import Promise
 from django.utils.timezone import get_default_timezone
 
-from django_filters import STRICTNESS, FilterSet
+from django_filters import FilterSet
 from django_filters.exceptions import FieldLookupError
 from django_filters.utils import (
     get_field_parts,
     get_model_field,
     handle_timezone,
     label_for_filter,
-    raw_validation,
     resolve_field,
+    translate_validation,
     verbose_field_name,
     verbose_lookup_expr
 )
@@ -342,19 +341,30 @@ class HandleTimezone(TestCase):
         self.assertEqual(handled, get_default_timezone().localize(dst_starting_date, True))
 
 
-class RawValidationDataTests(TestCase):
-    def test_simple(self):
-        class F(FilterSet):
-            class Meta:
-                model = Article
-                fields = ['id', 'author', 'name']
-                strict = STRICTNESS.RAISE_VALIDATION_ERROR
+class TranslateValidationDataTests(TestCase):
 
-        f = F(data={'id': 'foo', 'author': 'bar', 'name': 'baz'})
-        with self.assertRaises(ValidationError) as exc:
-            f.qs
+    class F(FilterSet):
+        class Meta:
+            model = Article
+            fields = ['id', 'author', 'name']
 
-        self.assertDictEqual(raw_validation(exc.exception), {
+    def test_error_detail(self):
+        f = self.F(data={'id': 'foo', 'author': 'bar', 'name': 'baz'})
+        exc = translate_validation(f.errors)
+
+        self.assertDictEqual(exc.detail, {
             'id': ['Enter a number.'],
             'author': ['Select a valid choice. That choice is not one of the available choices.'],
+        })
+
+    def test_full_error_details(self):
+        f = self.F(data={'id': 'foo', 'author': 'bar', 'name': 'baz'})
+        exc = translate_validation(f.errors)
+
+        self.assertEqual(exc.get_full_details(), {
+            'id': [{'message': 'Enter a number.', 'code': 'invalid'}],
+            'author': [{
+                'message': 'Select a valid choice. That choice is not one of the available choices.',
+                'code': 'invalid_choice',
+            }],
         })

django_filters/__init__.py
@@ -1,7 +1,6 @@
 # flake8: noqa
 import pkgutil
 
-from .constants import STRICTNESS
 from .filterset import FilterSet
 from .filters import *
 

django_filters/conf.py
@@ -2,7 +2,6 @@ from django.conf import settings as dj_settings
 from django.core.signals import setting_changed
 from django.utils.translation import ugettext_lazy as _
 
-from .constants import STRICTNESS
 from .utils import deprecate
 
 DEFAULTS = {
@@ -12,8 +11,6 @@ DEFAULTS = {
     'EMPTY_CHOICE_LABEL': '---------',
     'NULL_CHOICE_LABEL': None,
     'NULL_CHOICE_VALUE': 'null',
-
-    'STRICTNESS': STRICTNESS.RETURN_NO_RESULTS,
 
     'VERBOSE_LOOKUPS': {
         # transforms don't need to be verbose, since their expressions are chained

django_filters/constants.py
@@ -3,22 +3,3 @@ ALL_FIELDS = '__all__'
 
 
 EMPTY_VALUES = ([], (), {}, '', None)
-
-
-class STRICTNESS(object):
-    class IGNORE(object):
-        pass
-
-    class RETURN_NO_RESULTS(object):
-        pass
-
-    class RAISE_VALIDATION_ERROR(object):
-        pass
-
-    # Values of False & True chosen for backward compatability reasons.
-    # Originally, these were the only options.
-    _LEGACY = {
-        False: IGNORE,
-        True: RETURN_NO_RESULTS,
-        "RAISE": RAISE_VALIDATION_ERROR,
-    }

django_filters/filterset.py
@@ -7,7 +7,7 @@ from django.db.models.constants import LOOKUP_SEP
 from django.db.models.fields.related import ForeignObjectRel
 
 from .conf import settings
-from .constants import ALL_FIELDS, STRICTNESS
+from .constants import ALL_FIELDS
 from .filters import (
     BaseInFilter,
     BaseRangeFilter,
@@ -46,8 +46,6 @@ class FilterSetOptions(object):
         self.exclude = getattr(options, 'exclude', None)
 
         self.filter_overrides = getattr(options, 'filter_overrides', {})
-
-        self.strict = getattr(options, 'strict', None)
 
         self.form = getattr(options, 'form', forms.Form)
 
@@ -140,7 +138,7 @@ FILTER_FOR_DBFIELD_DEFAULTS = {
 class BaseFilterSet(object):
     FILTER_DEFAULTS = FILTER_FOR_DBFIELD_DEFAULTS
 
-    def __init__(self, data=None, queryset=None, *, request=None, prefix=None, strict=None):
+    def __init__(self, data=None, queryset=None, *, request=None, prefix=None):
         if queryset is None:
             queryset = self._meta.model._default_manager.all()
         model = queryset.model
@@ -151,16 +149,6 @@ class BaseFilterSet(object):
         self.request = request
         self.form_prefix = prefix
 
-        # What to do on on validation errors
-        # Fallback to meta, then settings strictness
-        if strict is None:
-            strict = self._meta.strict
-        if strict is None:
-            strict = settings.STRICTNESS
-
-        # transform legacy values
-        self.strict = STRICTNESS._LEGACY.get(strict, strict)
-
         self.filters = copy.deepcopy(self.base_filters)
 
         # propagate the model and filterset to the filters
@@ -168,42 +156,60 @@ class BaseFilterSet(object):
             filter_.model = model
             filter_.parent = self
 
+    def is_valid(self):
+        """
+        Return True if the underlying form has no errors, or False otherwise.
+        """
+        return self.is_bound and self.form.is_valid()
+
+    @property
+    def errors(self):
+        """
+        Return an ErrorDict for the data provided for the underlying form.
+        """
+        return self.form.errors
+
+    def filter_queryset(self, queryset):
+        """
+        Filter the queryset with the underlying form's `cleaned_data`. You must
+        call `is_valid()` or `errors` before calling this method.
+
+        This method should be overridden if additional filtering needs to be
+        applied to the queryset before it is cached.
+        """
+        for name, value in self.form.cleaned_data.items():
+            queryset = self.filters[name].filter(queryset, value)
+        return queryset
+
     @property
     def qs(self):
         if not hasattr(self, '_qs'):
-            if not self.is_bound:
-                self._qs = self.queryset.all()
-                return self._qs
-
-            if not self.form.is_valid():
-                if self.strict == STRICTNESS.RAISE_VALIDATION_ERROR:
-                    raise forms.ValidationError(self.form.errors)
-                elif self.strict == STRICTNESS.RETURN_NO_RESULTS:
-                    self._qs = self.queryset.none()
-                    return self._qs
-                # else STRICTNESS.IGNORE...  ignoring
-
-            # start with all the results and filter from there
             qs = self.queryset.all()
-            for name, filter_ in self.filters.items():
-                value = self.form.cleaned_data.get(name)
-
-                if value is not None:  # valid & clean data
-                    qs = filter_.filter(qs, value)
-
+            if self.is_bound:
+                # ensure form validation before filtering
+                self.errors
+                qs = self.filter_queryset(qs)
             self._qs = qs
-
         return self._qs
+
+    def get_form_class(self):
+        """
+        Returns a django Form suitable of validating the filterset data.
+
+        This method should be overridden if the form class needs to be
+        customized relative to the filterset instance.
+        """
+        fields = OrderedDict([
+            (name, filter_.field)
+            for name, filter_ in self.filters.items()])
+
+        return type(str('%sForm' % self.__class__.__name__),
+                    (self._meta.form,), fields)
 
     @property
     def form(self):
         if not hasattr(self, '_form'):
-            fields = OrderedDict([
-                (name, filter_.field)
-                for name, filter_ in self.filters.items()])
-
-            Form = type(str('%sForm' % self.__class__.__name__),
-                        (self._meta.form,), fields)
+            Form = self.get_form_class()
             if self.is_bound:
                 self._form = Form(self.data, prefix=self.form_prefix)
             else:

django_filters/rest_framework/backends.py
@@ -3,11 +3,12 @@ import warnings
 from django.template import loader
 
 from . import filters, filterset
-from .. import compat
+from .. import compat, utils
 
 
 class DjangoFilterBackend(object):
     default_filter_set = filterset.FilterSet
+    raise_exception = True
 
     @property
     def template(self):
@@ -49,8 +50,10 @@ class DjangoFilterBackend(object):
         filter_class = self.get_filter_class(view, queryset)
 
         if filter_class:
-            return filter_class(request.query_params, queryset=queryset, request=request).qs
-
+            filterset = filter_class(request.query_params, queryset=queryset, request=request)
+            if not filterset.is_valid() and self.raise_exception:
+                raise utils.translate_validation(filterset.errors)
+            return filterset.qs
         return queryset
 
     def to_html(self, request, queryset, view):

django_filters/rest_framework/filterset.py
@@ -1,12 +1,11 @@
 from copy import deepcopy
 
-from django import forms
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from django_filters import filterset
 
-from .. import compat, utils
+from .. import compat
 from .filters import BooleanFilter, IsoDateTimeFilter
 
 FILTER_FOR_DBFIELD_DEFAULTS = deepcopy(filterset.FILTER_FOR_DBFIELD_DEFAULTS)
@@ -38,12 +37,3 @@ class FilterSet(filterset.FilterSet):
             form.helper = helper
 
         return form
-
-    @property
-    def qs(self):
-        from rest_framework.exceptions import ValidationError
-
-        try:
-            return super(FilterSet, self).qs
-        except forms.ValidationError as e:
-            raise ValidationError(utils.raw_validation(e))

commit 36ac31251f723572ed5668dc88f09893fa6c0950

Populate error.message with error.params
Exceptions like ValidationError can include an optional 'params'
argument that contains parameters to format the 'message' argument with.
Handle these.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 78febd1 ("Rework validation, add queryset filter method (#788)")

django_filters/utils.py
@@ -317,7 +317,8 @@ def translate_validation(error_dict):
     from rest_framework.exceptions import ValidationError, ErrorDetail
 
     exc = OrderedDict(
-        (key, [ErrorDetail(e.message, code=e.code) for e in error_list])
+        (key, [ErrorDetail(
+            e.message % (e.params or {}), code=e.code) for e in error_list])
         for key, error_list in error_dict.as_data().items()
     )
 

tests/test_utils.py
@@ -8,7 +8,7 @@ from django.test import TestCase, override_settings
 from django.utils.functional import Promise
 from django.utils.timezone import get_default_timezone
 
-from django_filters import FilterSet
+from django_filters import FilterSet, ModelMultipleChoiceFilter
 from django_filters.exceptions import FieldLookupError
 from django_filters.utils import (
     MigrationNotice,
@@ -511,6 +511,13 @@ class TranslateValidationDataTests(TestCase):
             model = Article
             fields = ['id', 'author', 'name']
 
+    class G(FilterSet):
+        author = ModelMultipleChoiceFilter(queryset=User.objects.all())
+
+        class Meta:
+            model = Article
+            fields = ['id', 'author', 'name']
+
     def test_error_detail(self):
         f = self.F(data={'id': 'foo', 'author': 'bar', 'name': 'baz'})
         exc = translate_validation(f.errors)
@@ -519,6 +526,15 @@ class TranslateValidationDataTests(TestCase):
             'id': ['Enter a number.'],
             'author': ['Select a valid choice. That choice is not one of the available choices.'],
         })
+
+        f = self.G({'author': ['bar', 'baz']})
+        exc = translate_validation(f.errors)
+
+        # NOTE(stephenfin): Because we can't guarantee the order than filters
+        # are evaluated, we don't use 'assertDictEqual' here
+        self.assertEqual(set(exc.detail.keys()), {'author', })
+        self.assertRegex(str(exc.detail['author'][0]),
+                         '"ba[rz]" is not a valid value')
 
     def test_full_error_details(self):
         f = self.F(data={'id': 'foo', 'author': 'bar', 'name': 'baz'})