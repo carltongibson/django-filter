5381f4afbf9ce2b44c24eee4662d4f88a386a290
Carlton Gibson <carlton.gibson@noumenal.es>
2019-01-20 16:03:16 +0100

    Add testing against Django 2.2a1
    Just py37 for now.
    
    docs 2.1

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
