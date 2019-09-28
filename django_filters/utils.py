dbe2f91570a9707a3c26fb4c4b475610eb6dfd05
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2019-09-28 20:41:14 +0200

    Fixed import of FieldDoesNotExist.

--- django_filters/utils.py
+++ django_filters/utils.py
@@ -3,11 +3,10 @@ from collections import OrderedDict
 
 import django
 from django.conf import settings
-from django.core.exceptions import FieldError
+from django.core.exceptions import FieldDoesNotExist, FieldError
 from django.db import models
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.expressions import Expression
-from django.db.models.fields import FieldDoesNotExist
 from django.db.models.fields.related import ForeignObjectRel, RelatedField
 from django.utils import timezone
 from django.utils.encoding import force_str
