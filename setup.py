aa7f7ff508d07d6d6ebf28c5b3eaec00e0c09f3e
Bastien Vallet <bastien.vallet@gmail.com>
2019-10-29 16:49:51 +0100

    Declared support for, and added testing against, Python 3.8. (#1138)

--- setup.py
+++ setup.py
@@ -52,10 +52,11 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Framework :: Django',
     ],
     zip_safe=False,
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     install_requires=[
         'Django>=1.11',
     ],
