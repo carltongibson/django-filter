9feb87c321803f02c35fee18d058d0ecb3d241d5
David Smith <39445562+smithdc1@users.noreply.github.com>
2020-03-20 18:24:15 +0000

    Remove Django v1.9 support from runshell.py (#1190)

--- runshell.py
+++ runshell.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
-import django
 from django.core.management import execute_from_command_line
 
 
 def runshell():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    execute_from_command_line(
-        sys.argv[:1] +
-        ['migrate', '--noinput', '-v', '0'] +
-        (['--run-syncdb'] if django.VERSION >= (1, 9) else []))
-
-    argv = sys.argv[:1] + ['shell'] + sys.argv[1:]
-    execute_from_command_line(argv)
+    execute_from_command_line(sys.argv[:1] + ['migrate', '--noinput', '-v', '0'])
+    execute_from_command_line(sys.argv[:1] + ['shell'] + sys.argv[1:])
 
 if __name__ == '__main__':
     runshell()
