d9de80d827c85b4ee6853bd5718c848d36df8a67
Ryan P Kilby <kilbyr@gmail.com>
2020-03-20 11:14:29 -0700

    Simplify runshell commands

--- runshell.py
+++ runshell.py
@@ -6,13 +6,8 @@ from django.core.management import execute_from_command_line
 
 def runshell():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    execute_from_command_line(
-        sys.argv[:1] +
-        ['migrate', '--noinput', '-v', '0']
-    )
-
-    argv = sys.argv[:1] + ['shell'] + sys.argv[1:]
-    execute_from_command_line(argv)
+    execute_from_command_line(sys.argv[:1] + ['migrate', '--noinput', '-v', '0'])
+    execute_from_command_line(sys.argv[:1] + ['shell'] + sys.argv[1:])
 
 if __name__ == '__main__':
     runshell()
