013e7b246d273f28c845bf5863b5bf87d15bd1a4
Jelmer Draaijer <info@jelmert.nl>
2020-05-29 11:36:38 +0200

    Implement get_schema_operation_parameters for IsoDateTimeFromToRangeFilter

--- django_filters/rest_framework/filters.py
+++ django_filters/rest_framework/filters.py
@@ -11,3 +11,27 @@ class BooleanFilter(filters.BooleanFilter):
         kwargs.setdefault('widget', BooleanWidget)
 
         super().__init__(*args, **kwargs)
+
+
+class IsoDateTimeFromToRangeFilter(filters.IsoDateTimeFromToRangeFilter):
+    def get_schema_operation_parameters(self, field_name):
+        return [
+            {
+                "name": field_name + "_before",
+                "required": self.extra["required"],
+                "in": "query",
+                "description": self.label + " formatted according to ISO 8601"
+                if self.label is not None
+                else field_name,
+                "schema": {"type": "string", "format": "dateTime"},
+            },
+            {
+                "name": field_name + "_after",
+                "required": self.extra["required"],
+                "in": "query",
+                "description": self.label + " formatted according to ISO 8601"
+                if self.label is not None
+                else field_name,
+                "schema": {"type": "string", "format": "dateTime"},
+            },
+        ]
