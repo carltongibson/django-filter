5a2548afebba2d30f5fede12e7bf4e3f6ef16920
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2021-02-04 10:27:12 +0100

    Removed duplicated trove classifier. (#1322)

--- setup.py
+++ setup.py
@@ -56,7 +56,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Framework :: Django',
     ],
     zip_safe=False,
     python_requires='>=3.5',
