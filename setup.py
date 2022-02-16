dbc69e76cf829836e5c65ee4219d7f123fcc69ff
Amal Shaji <amalshajid@gmail.com>
2022-02-16 19:56:33 +0530

    Added testing for Python 3.10 (#1470)
    Co-authored-by: Carlton Gibson <carlton.gibson@noumenal.es>

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     zip_safe=False,
     python_requires='>=3.6',
