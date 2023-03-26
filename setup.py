de3019c6ca648387a125ff934719c276d76378bf
Carlton Gibson <carlton.gibson@noumenal.es>
2023-03-26 11:07:15 +0200

    Added Trove classifier for Django 4.2.

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
