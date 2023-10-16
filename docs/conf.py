0fa5feddc000f9b3292bb6a27c68593679567cd2
Jannis Vajen <jvajen@gmail.com>
2023-10-16 14:21:45 +0200

    Use Furo theme (#1614)

--- docs/conf.py
+++ docs/conf.py
@@ -94,7 +94,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -257,15 +257,3 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 # texinfo_show_urls = 'footnote'
-
-
-# see:
-# https://github.com/snide/sphinx_rtd_theme#using-this-theme-locally-then-building-on-read-the-docs
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
-
-# only import and set the theme if we're building docs locally
-if not on_rtd:
-    import sphinx_rtd_theme
-
-    html_theme = "sphinx_rtd_theme"
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
