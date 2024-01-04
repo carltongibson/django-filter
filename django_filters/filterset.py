97ec6578d41d5dd42f98363913e10dddf2025ba7
Shlomo Gordon <38845299+sgordon16@users.noreply.github.com>
2024-01-04 11:46:00 -0500

    Set default value of filterset data to MultiValueDict

--- django_filters/filterset.py
+++ django_filters/filterset.py
@@ -5,6 +5,7 @@ from django import forms
 from django.db import models
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.fields.related import ManyToManyRel, ManyToOneRel, OneToOneRel
+from django.utils.datastructures import MultiValueDict
 
 from .conf import settings
 from .constants import ALL_FIELDS
@@ -176,7 +177,7 @@ class BaseFilterSet:
         model = queryset.model
 
         self.is_bound = data is not None
-        self.data = data or {}
+        self.data = data or MultiValueDict()
         self.queryset = queryset
         self.request = request
         self.form_prefix = prefix
